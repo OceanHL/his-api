ae7343f10dc8f3f4bae7339e068ed88f38579458
OceanHL <876474290@qq.com>
2024-06-05 23:36:46 +0800

    启动类添加@MapperScan("com.example.his.api.db.mapper"), 使 MyBatis 可以自动扫描该包路径下的所有 Mapper 接口，并将它们注册为 Spring 容器中的 Bean

--- src/main/java/com/example/his/api/HisApiApplication.java
+++ src/main/java/com/example/his/api/HisApiApplication.java
@@ -1,9 +1,11 @@
 package com.example.his.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.his.api.db.mapper")
 public class HisApiApplication {
 
     public static void main(String[] args) {
