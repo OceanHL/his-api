77eb063cbc1eeba7fd87dc0a715067ea77cb8d09
OceanHL <876474290@qq.com>
2024-06-12 22:21:45 +0800

    新增MyBatis配置类MyBatisConfig.java

---
+++ src/main/java/com/example/his/api/config/MyBatisConfig.java
@@ -0,0 +1,41 @@
+package com.example.his.api.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * ClassName:
+ * Package: com.example.his.api.config
+ * Description: MyBatis配置类
+ *
+ * @Author Ocean_jhl
+ * @Create 2024/6/12 21:24
+ * @Version 1.0
+ */
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.example.his.api.db.mapper")
+public class MyBatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
