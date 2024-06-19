d0bde18fd9e673fffdb61b602b2119ad7a1bae6f
OceanHL <876474290@qq.com>
2024-06-19 23:11:42 +0800

    更新SearchUserByPageForm.java中的字段校验解释说明

--- src/main/java/com/example/his/api/mis/controller/form/SearchUserByPageForm.java
+++ src/main/java/com/example/his/api/mis/controller/form/SearchUserByPageForm.java
@@ -18,27 +18,27 @@ import org.hibernate.validator.constraints.Range;
  */
 @Data
 public class SearchUserByPageForm {
-    @NotNull(message = "页码不能为空")
+    @NotNull(message = "page页码不能为空")
     @Min(value = 1, message = "页码不能小于1")
     private Integer page;
 
-    @NotNull(message = "每页条数不能为空")
+    @NotNull(message = "length每页条数不能为空")
     @Range(min = 10, max = 50, message = "每页条数必须在10到50之间")
     private Integer length;
 
-    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{0,10}$", message = "姓名只能为简体中文")
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{0,10}$", message = "name姓名只能为简体中文")
     private String name;
 
-    @Pattern(regexp = "^男$|女$", message = "性别只能为男或女")
+    @Pattern(regexp = "^男$|女$", message = "sex性别只能为男或女")
     private String sex;
 
-    @Pattern(regexp = "^[a-zA-Z0-9\\u4e00-\\u9fa5]{2,10}$", message = "角色只能为英文和数字和简体中文")
+    @Pattern(regexp = "^[a-zA-Z0-9\\u4e00-\\u9fa5]{2,10}$", message = "role角色只能为英文和数字和简体中文")
     private String role;
 
-    @Min(value = 1, message = "部门ID不能小于1")
+    @Min(value = 1, message = "deptId部门ID不能小于1")
     private Integer deptId;
 
-    @Min(value = 1, message = "状态不能小于1")
-    @Max(value = 2, message = "状态不能大于2")
+    @Min(value = 1, message = "status状态不能小于1")
+    @Max(value = 2, message = "status状态不能大于2")
     private Integer status;
 }
